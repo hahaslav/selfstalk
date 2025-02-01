da4df3a606b3355d0b416a6b7c979cc4e09ca03f
hahaslav <yara-2004@ukr.net>
2025-02-01 21:07:34 +0200

    Take screenshot

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from mss.windows import MSS as mss
+from PIL import Image
+
 def take_screenshot():
-    pass
+    with mss() as sct:
+        new_screenshot = sct.grab(sct.monitors[0])
+        return Image.frombytes("RGB", new_screenshot.size, new_screenshot.bgra, "raw", "BGRX")
 
 if __name__ == '__main__':
-    take_screenshot()
+    screenshot = take_screenshot()
