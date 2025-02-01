56c7a2eec2d758b4d93f8deba668f9f9f3338d16
hahaslav <yara-2004@ukr.net>
2025-02-01 21:33:43 +0200

    Convert image to data string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mss.windows import MSS as mss
 from PIL import Image
+import base64
 
 def take_screenshot():
     with mss() as sct:
@@ -10,5 +11,11 @@ def resize_image(image, new_height):
     new_width = new_height * image.width // image.height
     return image.resize((new_width, new_height), Image.Resampling.BOX)
 
+def make_data_string():
+    with open("screenshots/1.png", 'rb') as fin:
+        image = fin.read()
+    return f"data:image/png;base64,{base64.b64encode(image).decode("utf-8")}"
+
 if __name__ == '__main__':
     screenshot = resize_image(take_screenshot(), 540)
+    screenshot.save("screenshots/1.png")
