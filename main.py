abcfc86795ec4f0b2dec67897926e236ce600d32
hahaslav <yara-2004@ukr.net>
2025-02-01 21:25:24 +0200

    Downscale screenshot

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from PIL import Image
 def take_screenshot():
     with mss() as sct:
         new_screenshot = sct.grab(sct.monitors[0])
-        return Image.frombytes("RGB", new_screenshot.size, new_screenshot.bgra, "raw", "BGRX")
+    return Image.frombytes("RGB", new_screenshot.size, new_screenshot.bgra, "raw", "BGRX")
+
+def resize_image(image, new_height):
+    new_width = new_height * image.width // image.height
+    return image.resize((new_width, new_height), Image.Resampling.BOX)
 
 if __name__ == '__main__':
-    screenshot = take_screenshot()
+    screenshot = resize_image(take_screenshot(), 540)
