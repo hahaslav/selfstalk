2ffb7db4a86da6a6f49de23e58bccbaaf8f32e92
hahaslav <yara-2004@ukr.net>
2025-02-01 21:51:43 +0200

    Add LLM analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mss.windows import MSS as mss
 from PIL import Image
 import base64
+import requests
 
 def take_screenshot():
     with mss() as sct:
@@ -16,6 +17,34 @@ def make_data_string():
         image = fin.read()
     return f"data:image/png;base64,{base64.b64encode(image).decode("utf-8")}"
 
+def chat(image_data_string):
+    response = requests.post(
+        "http://127.0.0.1:1234/v1/chat/completions",
+        headers={"Content-Type": "application/json"},
+        json={
+            "model": "minicpm-v-2_6",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "This is a screenshot. What is this user doing?"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": image_data_string}
+                        }
+                    ]
+                }
+            ],
+            "stream": False
+        }
+    ).json()
+
+    return response["choices"][0]["message"]["content"]
+
 if __name__ == '__main__':
     screenshot = resize_image(take_screenshot(), 540)
     screenshot.save("screenshots/1.png")
+    print(chat(make_data_string()))
